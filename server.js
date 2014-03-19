3532c68b2537c80867fb258e045cb93ba36cf57d
jiman <jiman.park@nhn.com>
2014-03-19 17:50:33 +0900

    init

---
+++ server.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Naver on 14. 3. 19.
+ *
+ * Author : UIT Development Gman.Park
+ *
+ * Simple Socket IO SyncCanvas
+ */
+
+
+var app = require('express')(), 
+server = require('http').createServer(app), 
+io = require('socket.io').listen(server);
+
+var port = Number(process.env.PORT || 5000);
+
+server.listen(port);
+
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + '/index.html');
+});
+
+
+//make connection
+io.sockets.on('connection', function(socket) {
+
+	socket.on('data-changed', function(packet) {
+		socket.broadcast.emit('data-changed',{
+			resData: packet.reqData
+		});
+	});
+
+});
+
+
+
