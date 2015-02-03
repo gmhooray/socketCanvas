18c5fb6a6e662001565d866e25607e93d0e002d8
박지만 <gman.park@nhn.com>
2015-02-03 20:28:20 +0900

    소스 수정

--- server.js
+++ server.js
@@ -6,29 +6,25 @@
  * Simple Socket IO SyncCanvas
  */
 
-
-var app = require('express')(), 
-server = require('http').createServer(app), 
-io = require('socket.io').listen(server);
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 var port = Number(process.env.PORT || 5000);
 
 server.listen(port);
 
-app.get('/', function(req, res) {
-	res.sendfile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/src/index.html');
 });
-
 
 //make connection
 io.sockets.on('connection', function(socket) {
-
-	socket.on('data-changed', function(packet) {
-		socket.broadcast.emit('data-changed',{
-			resData: packet.reqData
-		});
-	});
-
+	socket.on('data-changed', function(packet){
+        socket.broadcast.emit('data-changed',{
+            resData: packet.reqData
+        });
+    })
 });
 
 
